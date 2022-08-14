1e620b39da2a941a97c834d396042e62c4167e73
Amy Kim <amy.k7019@gmail.com>
2022-08-15 06:43:21 +0900

    fixed bugs with calculator added basic css

--- javascript.js
+++ javascript.js
@@ -1,24 +1,33 @@
-const display = document.querySelector("#display");
+const display = document.querySelector(".display");
+const disV1 = document.querySelector("#one");
+const disV2 = document.querySelector("#two");
+const opV = document.querySelector("#ops");
+
 let buttons = document.querySelector(".calculator");
 
 let numkey = document.querySelectorAll(".num-key");
 let operator = document.querySelectorAll(".operator");
 let equal = document.querySelector("#equal");
+let solution = document.querySelector(".solution");
 
 
-
+//value 1 is the input, value 2 is the output
 let value1 = 0;
 let value2 = 0;
-let op;
+let op = "";
+let init = 0;
+let toggle = true;
+let solve = 0;
 
-display.textContent = value1 + ", " + value2;
+disV1.textContent = value1;
+disV2.textContent = value2;
 
 const add = function (a, b) {
     return a + b;
 }
 
 const subtract = function (a, b) {
-    return b - a;
+    return a - b;
 }
 
 const multiply = function (a, b) {
@@ -26,7 +35,7 @@ const multiply = function (a, b) {
 }
 
 const divide = function (a, b) {
-    return b / a;
+    return a / b;
 }
 
 const mod = function (a, b) {
@@ -39,9 +48,9 @@ const operate = function(operator, a, b) {
             return (add(a, b));
         case "-":
             return(subtract(a, b));
-        case "*":
+        case "×":
             return(multiply(a, b));
-        case "/":
+        case "÷":
             return(divide(a, b));
         case "%":
             return(mod(a , b));
@@ -50,23 +59,39 @@ const operate = function(operator, a, b) {
 
 numkey.forEach(key => {
     key.addEventListener("click", function handleClick(event) {
-        value1 = parseInt(value1 * 10) + parseInt(key.value);
-        if (parseInt(key.value) == 0) value1 * 10; 
-        display.textContent = value1 + ", " + value2;
-        console.log("hi")
+            if(toggle) {
+                value1 = parseInt(value1 * 10) + parseInt(key.value);
+                if(parseInt(key.value) == 0) value1 * 10; 
+            } else {
+                value2 = parseInt(value2 * 10) + parseInt(key.value);
+                if(parseInt(key.value) == 0) value2 * 10; 
+            }
+            
+        disV1.textContent = value1;
+        disV2.textContent = value2;
+        console.log(value1 + ", " + value2);
     });
 });
+
 operator.forEach(key => {
     key.addEventListener("click", function handleClick(event) {
-        if(value2 == 0) {
-            value2 = value1;
-        }
+
+        toggle = false;
+
+        console.log(op + ", " + value1 + ", " + value2);
         if(key.value == "=") {
-            value2 = operate(op, value1, value2);
+            solve = operate(op, value1, value2);
+            disV1.textContent = value1;
+            disV2.textContent = value2;
+            value1 = solve;
+            value2 = 0;
         }
-        value1 = 0;
         op = key.value;
-        console.log(value1 + ", " + value2);
-        display.textContent = value1 + ", " + value2;
+        disV1.textContent = value1;
+            disV2.textContent = value2;
+        
+
+        solution.textContent = solve;
+        if(key.value != "=") opV.textContent = op;
     });
 });
